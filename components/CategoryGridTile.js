0dd6090549f3b2881366fc5f6c9176c8fe61b9f8
Don <don.qcastillo@yahoo.com>
2023-06-19 02:21:39 -0600

    darken the bg color of the app

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,11 +1,17 @@
-import { Pressable, Text, View, StyleSheet } from "react-native";
+import { Pressable, Text, View, StyleSheet, Platform } from "react-native";
 
 function CategoryGridTile({ title, color }) {
     return (
         <View style={styles.gridItem}>
-            <Pressable style={styles.button}>
-                <View style={styles.innerContainer}>
-                    <Text>{title}</Text>
+            <Pressable
+                style={({ pressed }) => [
+                    styles.button,
+                    pressed ? styles.buttonPressed : null,
+                ]}
+                android_ripple={{ color: "#ccc" }}
+            >
+                <View style={[styles.innerContainer, {backgroundColor: color}]}>
+                    <Text style={styles.title}>{title}</Text>
                 </View>
             </Pressable>
         </View>
@@ -21,19 +27,28 @@ const styles = StyleSheet.create({
         height: 150,
         borderRadius: 8,
         elevation: 4,
-        backgroundColor: 'white',
+        backgroundColor: "white",
         shadowColor: "black",
         shadowOpacity: 0.25,
-        shadowOffset: {width: 0, height: 2},
-        shadowRadius: 8
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 8,
+        overflow: Platform.OS === "android" ? "hidden" : "visible",
     },
     button: {
-        flex: 1
+        flex: 1,
+    },
+    buttonPressed: {
+        opacity: 0.5,
     },
     innerContainer: {
         flex: 1,
         padding: 16,
-        justifyContent: 'center',
-        alignContent: 'center'
-    }
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 8,
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 18,
+    },
 });
