2b23b5f917e5bce80b69e6218a1511aa56e015ad
Don <don.qcastillo@yahoo.com>
2023-06-28 04:31:27 -0600

    added nested navigation

--- App.js
+++ App.js
@@ -2,11 +2,33 @@ import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+    return (
+        <Drawer.Navigator
+            screenOptions={{
+                headerStyle: { backgroundColor: "#351401" },
+                headerTintColor: "white",
+                sceneContainerStyle: { backgroundColor: "#3f2f25" },
+            }}
+        >
+            <Drawer.Screen
+                name="Categories"
+                component={CategoriesScreen}
+                options={{ title: "All Categories" }}
+            />
+            <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+        </Drawer.Navigator>
+    );
+}
 
 export default function App() {
     return (
@@ -21,10 +43,10 @@ export default function App() {
                     }}
                 >
                     <Stack.Screen
-                        name="MealsCategories"
-                        component={CategoriesScreen}
+                        name="Drawer"
+                        component={DrawerNavigator}
                         options={{
-                            title: "All Categories",
+                            headerShown: false,
                         }}
                     />
                     <Stack.Screen
@@ -40,6 +62,9 @@ export default function App() {
                     <Stack.Screen
                         name="MealDetails"
                         component={MealDetailScreen}
+                        options={{
+                            title: "About the Meal"
+                        }}
                     />
                 </Stack.Navigator>
             </NavigationContainer>
